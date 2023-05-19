9c4ba30ce4933dd7e8ac62368770dba71e617b5c
Adam Jankowski <ghost17814@gmail.com>
2023-05-19 12:20:21 -0400

    ugh java

--- app.js
+++ app.js
@@ -4,7 +4,54 @@ console.log(
 );
 
 // Exercise 1
+/* const firstName = "Big"; */
+let lastName;
+var age; 
 
 // Exercise 2
+const firstName = "Adam";
+lastName = "Jankowski";
+var age = "33";
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+/* 
+3
+1. Declare a variable named `language` using the `let` keyword, and assign it the value `"JavaScript"` (string)
+2. Declare a variable named `createdYear` using the `let` keyword, and assign it the value `1995` (number)
+3. Declare a variable named `isCaseSensitive` using the `let` keyword, and assign it the value `true` (boolean)
+
+4
+1. Declare a variable named `price` using the `let` keyword, and assign it the value `19.99` (number)
+2. Declare a variable named `isOnSale` using the `let` keyword, and assign it the value `false` (boolean)
+3. Declare a variable named `salePercentage` using the `let` keyword, and assign it the value `15` (number)
+4. Declare a variable named `stock` using the `let` keyword, and assign it the value `0` (number)
+5. Declare a variable named `inStock` using the `let` keyword, and assign it the value `false` (boolean)
+6. Declare a variable named `selectedSize` using the `let` keyword, and assign it the value `"M"` (string)
+
+
+5
+1. Declare a variable named `title` using the `let` keyword, and assign it the value `"Name of the Wind"` (string)
+2. Declare a variable named `author` using the `let` keyword, and assign it the value `"Patrick Rothfuss"` (string)
+3. Declare a variable named `pageCount` using the `let` keyword, and assign it the value `722` (number)
+4. Declare a variable named `bookmark` using the `let` keyword, and assign it the value `456` (number)
+5. Declare a variable named `hasRead` using the `let` keyword, and assign it the value `true` (boolean)
+*/
